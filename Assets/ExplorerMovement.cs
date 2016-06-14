5a4412671388bd56faec4a69e018e8569a6f8342
Jim Eldred <gscjim@gmail.com>
2016-06-13 20:29:29 -0500

    initial commit, trying to avoid large files

---
+++ Assets/ExplorerMovement.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplorerMovement : MonoBehaviour
+{
+
+    bool new_turn;
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Debug String");
+        new_turn = true;
+    }
+
+    // Update is called once per frame
+    public void NextTurn ()
+    {
+        new_turn = true;
+    }
+
+    void Update()
+    {
+
+        float movex = Input.GetAxis("Horizontal");
+        float movey = Input.GetAxis("Vertical");
+
+        if (Input.GetKey(KeyCode.N))
+        {
+            new_turn = true;
+        }
+        
+        float move = 2.55f;
+        if (movex > 0 && new_turn)
+        {
+            gameObject.transform.Translate(move, 0, 0);
+            new_turn = false;
+            Debug.Log(movex.ToString());
+        }
+        else if (movex< 0 && new_turn)
+        {
+            gameObject.transform.Translate(-move, 0, 0);
+            Debug.Log(movex.ToString());
+            new_turn = false;
+        }
+        if (movey > 0 && new_turn)
+        {
+            gameObject.transform.Translate(0, move, 0);
+            new_turn = false;
+            Debug.Log(movex.ToString());
+        }
+        else if (movey < 0 && new_turn)
+        {
+            gameObject.transform.Translate(0, -move, 0);
+            Debug.Log(movex.ToString());
+            new_turn = false;
+        }
+    }
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControllerScript : MonoBehaviour
+{
+    
+   
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Debug String");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        float movex = Input.GetAxis("Horizontal");
+        float movey = Input.GetAxis("Vertocal");
+        Debug.Log(movex.ToString());
+        Debug.Log("Debug String");
+        float x_move = 2.55f;
+        if (movex > 0)
+        {
+            gameObject.transform.Translate(x_move, 0, 0);
+        }
+    }
+
+}
